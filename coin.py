91d7025309cf0c79bf55dc68a575260c5829b656
Hiromu15 <murohiigana6@gmail.com>
2024-06-28 10:52:12 +0900

    Hot:init revision

---
+++ coin.py
@@ -0,0 +1,17 @@
+import random
+
+cnt = 0
+
+print("Tossing a coin...")
+for i in range(3):
+    judge = random.randint(0, 100)
+    if(judge % 2 == 0):
+        S = "Heads"
+        cnt += 1
+    else:
+        S = "Tails"
+    print("Round", i, end = "")
+    print(":", S)
+
+print("Heads:", cnt, end = "")
+print(", Tails:", 3 - cnt)
